f4259e75bbd2fa214c0b913434b75717a89c84b9
Muhammed Abdelnasser <65632980+mohamdnasser@users.noreply.github.com>
2021-04-22 19:25:56 +0200

    #6 Added Create And Update Time Stamp

--- back-end/src/main/java/com/spring/restaurant/model/CategoryOrder.java
+++ back-end/src/main/java/com/spring/restaurant/model/CategoryOrder.java
@@ -3,9 +3,12 @@ package com.spring.restaurant.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import java.util.Date;
 
 // Id
 // name
@@ -14,6 +17,17 @@ import javax.persistence.MappedSuperclass;
 @AllArgsConstructor
 @MappedSuperclass
 public class CategoryOrder extends BaseEntity{
+
     @Column(name = "name")
     private String name;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date dateCreated;
+
+    @Column(name = "date_updated")
+    @UpdateTimestamp
+    private Date dateUpdated;
+
+
 }
