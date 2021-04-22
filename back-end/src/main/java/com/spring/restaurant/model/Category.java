ced7b383aad6a68d0739ca382fcfe94476086e0a
Muhammed Abdelnasser <65632980+mohamdnasser@users.noreply.github.com>
2021-04-22 19:16:10 +0200

    #5 Relation Between Models Added

--- back-end/src/main/java/com/spring/restaurant/model/Category.java
+++ back-end/src/main/java/com/spring/restaurant/model/Category.java
@@ -2,14 +2,20 @@ package com.spring.restaurant.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.engine.spi.Managed;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
 @Entity
 @Table(name = "category")
 public class Category extends CategoryOrder {
+
+    @OneToMany(mappedBy = "category")
+    private Set<Order> Orders ;
 
 }
