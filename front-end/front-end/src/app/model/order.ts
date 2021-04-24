1895b0a53d4d261957f64abd625c55666b890efa
Muhammed Abdelnasser <65632980+mohamdnasser@users.noreply.github.com>
2021-04-24 17:51:47 +0200

    #5 Images Added.

--- front-end/front-end/src/app/model/order.ts
+++ front-end/front-end/src/app/model/order.ts
@@ -1,14 +1,23 @@
 export class Order {
 
-    public id: Number; 
-    public name: String;
-    public description: String;
-    public image: String;
-    public price: Number;
-    public data_create: Date;
-    public data_update: Date;
+    id: Number; 
+    name: String;
+    description: String;
+    img: String;
+    price: Number;
+    data_create: Date;
+    data_update: Date;
 
 
+    constructor(id: Number, name: String, description: String, img: String, price: Number, data_create: Date, data_update: Date  ){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.img = img;
+        this.price = price;
+        this.data_create = data_create;
+        this.data_update = data_update;
+    }
 
 
 }
