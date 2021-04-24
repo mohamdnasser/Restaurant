4965bcdbd1e6811142f2ffd59e8779cf0563063f
Muhammed Abdelnasser <65632980+mohamdnasser@users.noreply.github.com>
2021-04-24 05:28:09 +0200

    #3 Order Component Added.

---
+++ front-end/front-end/src/app/components/orderitems/orderitems.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/model/order';
+import { OrderServiceService } from 'src/app/service/order-service.service';
+
+@Component({
+  selector: 'app-orderitems',
+  templateUrl: './orderitems.component.html',
+  styleUrls: ['./orderitems.component.css']
+})
+export class OrderitemsComponent implements OnInit {
+
+  private ordersArray: Order[] = [];
+
+  constructor(private orderService: OrderServiceService) { }
+
+  ngOnInit(): void {
+    this.getOrders();
+  }
+
+
+  getOrders(){
+    this.orderService.getOrders().subscribe(
+      data => this.ordersArray = data
+    )
+  }
+
+}
