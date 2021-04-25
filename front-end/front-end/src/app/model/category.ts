76fa1e3d3391e8624106461f1399cc8f5bdc8b8e
Muhammed Abdelnasser <65632980+mohamdnasser@users.noreply.github.com>
2021-04-25 12:46:31 +0200

    #9

--- front-end/front-end/src/app/model/category.ts
+++ front-end/front-end/src/app/model/category.ts
@@ -1,8 +1,18 @@
 export class Category {
 
-    public id: Number; 
-    public name: String;
-    public data_create: Date;
-    public data_update: Date;
+    id: Number; 
+    name: String;
+    logo: String;
+    data_create: Date;
+    data_update: Date;
+
+
+    constructor(id: Number, name: String,  logo: String, description: String, img: String, price: Number, data_create: Date, data_update: Date  ){
+        this.id = id;
+        this.name = name;
+        this.logo = logo;
+        this.data_create = data_create;
+        this.data_update = data_update;
+    }
 
 }
